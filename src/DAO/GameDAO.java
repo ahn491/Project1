fceac94a371a9b03b083ab795c9359d45913efb1
user <user@DESKTOP-7EQF3M8>
2024-07-10 10:01:03 +0900

    FINAL COMMIT!

--- src/DAO/GameDAO.java
+++ src/DAO/GameDAO.java
@@ -11,8 +11,8 @@ import VO.GameVO;
 
 public class GameDAO {
 	Connection con;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
+	PreparedStatement pstmt;
+	ResultSet rs;
 	
 	public GameDAO() throws ClassNotFoundException, SQLException {
 		con = new DBConnection().getConnection();
@@ -61,12 +61,12 @@ public class GameDAO {
 		return true;
 	}
 	
-	public ArrayList<GameVO> getSumScore() throws SQLException { // 합산된 게임 점수(ArrayList 형태로 값 저장)
+	public ArrayList<GameVO> getSumScore() throws SQLException { // 평균 게임 점수(ArrayList 형태로 값 저장)
 		ArrayList<GameVO> list = new ArrayList<GameVO>();
 		
 		StringBuffer sql = new StringBuffer();
 		
-		// 게임1(수학 연산 게임)의 총 합산 점수와 게임2(Hangman 게임)의 총 합산 점수를 더한 값과 ID를 출력하는 Query문
+		// 모든 게임의 평균 점수와 ID를 출력하는 Query문
 		sql.append("SELECT m.id, (AVG(ms.score1) + AVG(ms.score2)) / 2 avg_game \n");
 		sql.append("FROM member m, member_score ms 								\n");
 		sql.append("WHERE m.id = ms.id 											\n");
