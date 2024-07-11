757224d39cabee7083558cf7210f3ca6f94d3a30
user <user@DESKTOP-7EQF3M8>
2024-07-11 13:39:44 +0900

    REAL FINAL COMMIT!!!!!!!

--- src/WindowBuilder/Main_Page.java
+++ src/WindowBuilder/Main_Page.java
@@ -15,9 +15,15 @@ import javax.swing.border.EmptyBorder;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.CategoryItemLabelGenerator;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.TextAnchor;
 
 import DAO.GameDAO;
 import VO.GameVO;
@@ -165,7 +171,7 @@ public class Main_Page extends JFrame {
 		JButton btn_chart = new JButton(""); // 게임 점수 합산 버튼
 		
 		btn_chart.setBorderPainted(false); 
-		btn_chart.setContentAreaFilled(false); 
+		btn_chart.setContentAreaFilled(false);
 		btn_chart.setFocusPainted(false);
 		
 		btn_chart.addMouseListener(new MouseAdapter() {
@@ -177,25 +183,84 @@ public class Main_Page extends JFrame {
 					DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 					
 					for(GameVO game : list) { // 차트에 표시될 값을 저장
-						dataset.addValue(game.getSum(), "합산 점수", game.getId()); // 차트 요소 설정(값, 범례, 항목)
+						dataset.addValue(game.getSum(), "평균 점수", game.getId()); // 차트 요소 설정(값, 범례, 항목)
 					}
 					
 					PlotOrientation po = PlotOrientation.HORIZONTAL; // 차트 방향(HORIZONTAL : 가로축과 세로축을 그대로 유지, VERTICAL : 가로축과 세로축 변환)
 					
 					// 차트 생성(3D 막대형 차트 - 제목, 가로축 제목, 세로축 제목, 차트 요소, 차트 방향, ...)
-					JFreeChart chart = ChartFactory.createBarChart3D("게임점수 순위", "유저", "점수", dataset, po, true,true,true);
+					JFreeChart chart = ChartFactory.createBarChart3D("게임점수 순위", "", "", dataset, po, true,true,true);
 					
 					CategoryPlot plot = chart.getCategoryPlot();
-					
+
 					chart.getTitle().setFont(new Font("맑은 고딕", Font.BOLD, 15)); // 차트 제목 설정
 					
 					chart.getLegend().setItemFont(new Font("맑은 고딕", Font.BOLD, 15)); // 차트 범례 설정
+					
+					chart.removeLegend(); // 차트 범례 제거
 					
 					plot.getDomainAxis().setLabelFont(new Font("맑은 고딕", Font.BOLD, 15)); // Y축 Label 설정
 					
 					plot.getRangeAxis().setLabelFont(new Font("맑은 고딕", Font.BOLD, 15)); // X축 Label 설정
 					
-					chart.setBackgroundPaint(Color.WHITE); // 차트 배경색 지정
+					plot.setRangeGridlinesVisible(false); // 차트 영역 안 X축 눈금 제거
+					
+					plot.getRangeAxis().setVisible(false); // X축 제거
+					
+					plot.getDomainAxis().setAxisLineVisible(false); // Y축 눈금선 제거
+					
+					ItemLabelPosition position1 = new ItemLabelPosition(ItemLabelAnchor.OUTSIDE3, TextAnchor.CENTER_LEFT); // 값 Label 위치
+					
+					BarRenderer renderer = new BarRenderer();
+					
+					renderer.setBaseItemLabelGenerator(new CategoryItemLabelGenerator() {
+						@Override
+						public String generateRowLabel(CategoryDataset dset, int row) {
+							return null;
+						}
+						
+						@Override
+						public String generateLabel(CategoryDataset dset, int row, int column) { // 표시할 값 Label 내용 + 막대별로 다르게 색상 설정
+							switch(column) {
+								case 0 :
+									renderer.setPaint(Color.BLUE);
+									break;
+									
+								case 1 :
+									renderer.setPaint(Color.CYAN);
+									break;
+									
+								case 2 :
+									renderer.setPaint(Color.GREEN);
+									break;
+									
+								case 3 :
+									renderer.setPaint(Color.YELLOW);
+									break;
+									
+								case 4 :
+									renderer.setPaint(Color.RED);
+									break;
+							}
+							
+							return Integer.toString(list.get(column).getSum()); // 표시할 값 Label
+						}
+						
+						@Override
+						public String generateColumnLabel(CategoryDataset dset, int column) {
+							return null;
+						}
+					});
+					
+					renderer.setBasePositiveItemLabelPosition(position1); // 값 Label 위치 설정
+					renderer.setBaseItemLabelsVisible(true); // 값 Label 표시
+					renderer.setBaseItemLabelFont(new Font("맑은 고딕", Font.BOLD, 15)); // 값 Label 글꼴 설정
+					
+					plot.setRenderer(renderer);
+					
+					plot.setBackgroundPaint(Color.decode("#F1EBDE")); // 차트 막대 영역 배경색 지정
+					
+					chart.setBackgroundPaint(Color.decode("#F1EBDE")); // 차트 배경색 지정
 					
 					ChartFrame cf = new ChartFrame("게임 점수 합산 결과", chart); // Chart Frame 생성 및 Chart Frame에 차트 추가
 					
